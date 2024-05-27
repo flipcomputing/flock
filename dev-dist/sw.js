07a181e02f9c52f8b75aeb48cd4dc74d6e42d099
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2024-05-27 16:32:21 +0000

    Added icons

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.97oi8l6kn18"
+    "revision": "0.37hjrm8o8b"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,47 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default {
      plugins: [
-         VitePWA({
-           registerType: 'autoUpdate',
-           devOptions: {
-             enabled: true
-           }
-         })
+       VitePWA({
+         registerType: 'autoUpdate',
+            devOptions: {
+              enabled: true
+            },
+         includeAssets: ['/images/favicon.ico', '/images/favicon.svg', '/images/apple-touch-icon.png', '/images/mask-icon.svg'],
+         manifest: {
+           name: 'Flock 3D Blocks',
+           short_name: 'Flock',
+           description: 'Create 3D apps with blocks',
+           theme_color: '#800080',
+           start_url: '.',
+           icons: [
+             {
+               src: '/images/icon_192x192.png',
+               sizes: '192x192',
+               type: 'image/png'
+             },
+             {
+               src: '/images/icon_512x512.png',
+               sizes: '512x512',
+               type: 'image/png'
+             }
+           ]
+         },
+         workbox: {
+           runtimeCaching: [
+             {
+               urlPattern: ({ request }) => request.destination === 'images',
+               handler: 'CacheFirst',
+               options: {
+                 cacheName: 'images-cache',
+                 expiration: {
+                   maxEntries: 10,
+                   maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+                 },
+               },
+             },
+           ],
+         },
+       })
        ],
   server: {
 	   host: '0.0.0.0',
