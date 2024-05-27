962cd588d30d145da0cb893ea939c27101377a99
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2024-05-27 15:32:03 +0000

    Added vite pwa

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,14 @@
+import { VitePWA } from 'vite-plugin-pwa'
+
 export default {
+     plugins: [
+         VitePWA({
+           registerType: 'autoUpdate',
+           devOptions: {
+             enabled: true
+           }
+         })
+       ],
   server: {
 	   host: '0.0.0.0',
   }
