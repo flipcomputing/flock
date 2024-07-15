9022b264bb93fffe3349cf3109a4446c60dfd5ee
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2024-07-15 08:55:02 +0000

    Converted toolbox to json

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-07658ed7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1c5hld51oco"
+    "revision": "0.bj7c2jaihu8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
