fee0555d732d9bcb7d8bb6655b2bdef0f0a1e998
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2024-07-15 09:34:26 +0000

    Snippet creates variable

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-07658ed7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bj7c2jaihu8"
+    "revision": "0.kcp54qhr9q"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
