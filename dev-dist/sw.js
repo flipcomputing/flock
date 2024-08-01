48e1d9b61d6f49f50fd95378a079f55e8674bf91
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2024-08-01 15:14:01 +0000

    Added a character animation example

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-07658ed7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.o0c8i9u2jog"
+    "revision": "0.lopt24dju2o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
