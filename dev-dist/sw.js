f1b43618807f378d30113241f06ba8728c0b6e23
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2024-08-11 11:27:12 +0000

    Updated font size in beetle to match new settings

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-07658ed7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h64k05qs0h8"
+    "revision": "0.3js6i0gc23"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
