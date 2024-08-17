012bc3527725491b0f5bcec4f0a36ad9ca5e3df1
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2024-08-17 19:33:17 +0000

    Added roominator example

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-07658ed7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vno4ha2v8l8"
+    "revision": "0.atiojcjcd1g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
