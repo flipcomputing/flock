4c0fb05ceda4766338f3b884e01483272e3487bb
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2024-08-27 14:05:36 +0000

    Added opening card example

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-07658ed7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b8pf9prv7d8"
+    "revision": "0.pi9fkk9q9u8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
