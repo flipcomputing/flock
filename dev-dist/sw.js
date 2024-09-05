05c2f5c5e814e8e0165c4594e3509609daf69c6a
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2024-09-05 07:17:09 +0000

    Use transform node for say block so that it doesn't scale with the parent

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-07658ed7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.f3dgpntb0to"
+    "revision": "0.751uivm7r2o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
