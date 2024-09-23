f768eca07b6519a62f0978aaa2b7f45581320718
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2024-09-23 20:35:03 +0000

    Update title to Flock XR

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-07658ed7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q85vmq2u7k8"
+    "revision": "0.8dae61q7oj"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
