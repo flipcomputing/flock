20681b75b021c315d1b0923fb7fc51641acbaf0d
Tracy Gardner <5155794-TracyGardner@users.noreply.replit.com>
2024-09-28 16:08:20 +0000

    Swapped buttons to give more room to shape menu

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-07658ed7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1opn3d1qeog"
+    "revision": "0.cc9joqbacj8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
