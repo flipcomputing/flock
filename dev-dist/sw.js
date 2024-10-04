001829ac505c3a18fc67b2b2761444bc59fd99a0
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2024-10-04 16:45:40 +0000

    PWA updates

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-07658ed7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qjq2t57h718"
+    "revision": "0.qqlo12sreno"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
