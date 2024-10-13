27734244e3c8204557d686523855f2ed3586e120
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2024-10-13 19:41:06 +0000

    Package updates

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-07658ed7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.thu4ortsjfg"
+    "revision": "0.ei0vkdkfcg8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
