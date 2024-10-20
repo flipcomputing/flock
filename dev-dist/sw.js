001eae3b3bc24e0f85553088d7598b6207a8c132
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2024-10-20 05:55:32 +0000

    Panel height fix on mobile

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-07658ed7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.l3hcmc6han"
+    "revision": "0.jk1jimklap"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
