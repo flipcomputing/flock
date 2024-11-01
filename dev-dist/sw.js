857722669c68595b568f3ad21b4c0da50c257564
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2024-11-01 09:25:18 +0000

    Updated info panel

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.c9n9em9qr08"
+    "revision": "0.3735h9npnoo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
