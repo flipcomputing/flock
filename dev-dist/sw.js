d1c9e1770f69857b8bba576428233e8b50a1d9aa
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2024-11-30 18:10:07 +0000

    Added to animations example

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.62koj1umrpo"
+    "revision": "0.pus59tcv18"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
