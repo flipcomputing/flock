b46e861f530b6c250426dd5eb829718c20f179b1
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2024-12-05 16:10:47 +0000

    Added snow globe

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4haulnss49"
+    "revision": "0.vhs238994a8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
