934c99da1e93c2608c0fd6bdebdfe58774750f49
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2025-01-05 15:00:12 +0000

    Updated controller starter to use dynamic if else block

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ushn7ah7618"
+    "revision": "0.04dg4ida8og"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
