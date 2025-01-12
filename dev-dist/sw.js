e8d0ef8eb823868a51a401fa960310cd5ec701ca
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2025-01-12 07:25:53 +0000

    Moved toolbox categories to separate variables

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.d9kcu87hlm8"
+    "revision": "0.17snahge3r"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
