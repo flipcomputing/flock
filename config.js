8913accf372c710ce6d618488a7678e00cd1bde1
Liz Smart <12066983+libxx1@users.noreply.github.com>
2025-01-13 17:11:21 +0000

    Update config.js
    updating number of hut3 colours

--- config.js
+++ config.js
@@ -106,7 +106,7 @@ export const objectColours = {
 	"tree_fat.glb": ["#66CDAA", "#CD853F"],
 	"hut.glb": ["#814C22", "#231E1D", "#FFF4EC", "#E7AF3A", "#E72243", "#878787"],
 	"hut2.glb": ["#00BFFF", "#FFD700", "#FF4500", "#8A2BE2", "#2C28E7", "#878787"],
-	"hut3.glb": ["#50C878", "#FFBF00", "#FE5A1D", "#B09B6B", "#878787"],
+	"hut3.glb": ["#50C878", "#FFBF00", "#FE5A1D", "#50C878", "#FFBF00", "#B09B6B", "#878787"],
 	"hut4.glb": ["#FFFCF0", "#FF89DB", "#E7223B", "#FFA673", "#3E8AE7", "#3AE744", "#878787"],
 };
 
