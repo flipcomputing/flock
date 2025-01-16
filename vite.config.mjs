6e26a0adae7daeeeb1e1ce551b81560a8d515595
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2025-01-16 08:40:05 +0000

    Deploy tutorial file

--- vite.config.mjs
+++ vite.config.mjs
@@ -190,6 +190,7 @@ export default {
       name: 'copy-library-files',
       writeBundle() {
         copyFileSync('example.html', 'dist/example.html');
+        copyFileSync('tutorial.html', 'dist/tutorial.html');
       },
     },
   ],
