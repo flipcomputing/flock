f06e870ec52da3958759a171f4c5d7bcd3a2d56c
Liz Smart <12066983+libxx1@users.noreply.github.com>
2025-02-18 14:33:48 +0000

    Update config.js

--- config.js
+++ config.js
@@ -112,8 +112,8 @@ export const objectColours = {
 	"Gem2.glb": ["#8A2BE2"], // Blue Violet
 	"Gem3.glb": ["#FF4500"], // Orange Red
 	"tree.glb": ["#66CDAA", "#CD853F"],
-	"tree2.glb": ["#4CBB17", "#A1623B"],
-	"tree3.glb": ["#7F9F7F", "#D8D8D8"],
+	"tree2.glb": ["#7F9F7F", "#A1623B"],
+	"tree3.glb": ["#4CBB17", "#D8D8D8"],
 	"tree4.glb": ["#0D5B28", "#6D6C51"],
 	"rocks.glb": ["#898D86", "#99a83d"],
 	"rocks2.glb": ["#898D86", "#99a83d"],
