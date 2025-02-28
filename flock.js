2f11e99d21f7322d5a9756fc37e0c59582d20a06
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2025-02-28 16:54:59 +0000

    Tidy up physics removal from rotate

--- flock.js
+++ flock.js
@@ -3989,17 +3989,6 @@ export const flock = {
 				return;
 			}
 
-			/*if (mesh.physics) {
-				if (
-					mesh.physics.getMotionType() !==
-					flock.BABYLON.PhysicsMotionType.DYNAMIC
-				) {
-					mesh.physics.setMotionType(
-						flock.BABYLON.PhysicsMotionType.ANIMATED,
-					);
-				}
-			}*
-
 			const incrementalRotation =
 				flock.BABYLON.Quaternion.RotationYawPitchRoll(
 					flock.BABYLON.Tools.ToRadians(y),
