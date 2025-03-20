596b2a309dea75848a03055b96a71aee649a5790
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2025-03-20 07:56:01 +0000

    Set audioEngine true for breaking change in babylon js

--- flock.js
+++ flock.js
@@ -313,6 +313,7 @@ export const flock = {
 		flock.engine = new flock.BABYLON.Engine(flock.canvas, true, {
 			stencil: true,
 			deterministicLockstep: true,
+			audioEngine: true
 		});
 
 		flock.engine.enableOfflineSupport = false;
