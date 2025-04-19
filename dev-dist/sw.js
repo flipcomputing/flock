b8d34affa2f0c433d3299567f20397ed2d6275e9
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2025-04-19 06:17:15 +0000

    Hide bounds and focus buttons

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a0ndp36inco"
+    "revision": "0.72clm1a2h2"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
