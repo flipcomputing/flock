43aaf4f722607edd5b7730f78c23ca46c67f0840
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2025-04-20 06:20:49 +0000

    Updated info and link

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -91,7 +91,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hbbk3uf4r98"
+    "revision": "0.p2fd7nvvkh8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
