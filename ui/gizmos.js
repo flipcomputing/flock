f7f481d52cd6157c0ee09efa88f4c0800b9475fb
Zishan Rahman <zishanfrahman@outlook.com>
2025-09-09 19:02:31 +0100

    Update sky block with colour everytime sky colour is set with colour picker gizmo

--- ui/gizmos.js
+++ ui/gizmos.js
@@ -116,6 +116,7 @@ function applyColorAtPosition(canvasX, canvasY) {
     updateBlockColorAndHighlight(pickResult.pickedMesh, selectedColor);
   } else {
     flock.setSky(selectedColor);
+    updateBlockColorAndHighlight(null, selectedColor);
   }
 }
 
