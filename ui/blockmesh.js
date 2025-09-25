4db5547705a7f1223614f51ac03fd5373bb9ccd4
Zishan Rahman <zishanfrahman@outlook.com>
2025-09-25 19:18:42 +0100

    Assign value returned by createBlockWithShadows to block variable so that it gets used

--- ui/blockmesh.js
+++ ui/blockmesh.js
@@ -1867,7 +1867,7 @@ export function updateBlockColorAndHighlight(mesh, selectedColor) {
       // if (connection) {
       //   connection.connect(block.previousConnection);
       // }
-      createBlockWithShadows("set_sky_color", null, selectedColor);
+      block = createBlockWithShadows("set_sky_color", null, selectedColor);
     }
     withUndoGroup(() => {
       const found = findNestedColorTarget(block);
