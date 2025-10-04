5f66753d7ed75e900da0bf1c90356518bb7f1efd
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2025-10-04 07:54:03 +0000

    Moved block characters to the end of the configuration list

--- config.js
+++ config.js
@@ -72,10 +72,6 @@ export const audioNames = [
 ];
 
 export const characterNames = [
-	"Character1.glb",
-	"Character2.glb",
-	"Character3.glb",
-	"Character4.glb",
 	"Liz1.glb",
 	"Liz2.glb",
 	"Liz3.glb",
@@ -87,6 +83,10 @@ export const characterNames = [
 	"Block4.glb",
 	"Block5.glb",
 	"Block6.glb",
+	"Character1.glb",
+	"Character2.glb",
+	"Character3.glb",
+	"Character4.glb",
 ];
 
 export const multiObjectNames = [
@@ -110,7 +110,7 @@ export const multiObjectNames = [
 	"humped.glb",
 	"jetty.glb",
 	"flower.glb",
-	"flower2.glb"
+	"flower2.glb",
 	//"stones_straight.glb",
 	//"stones_curve.glb",
 ];
@@ -127,8 +127,8 @@ export const objectNames = [
 	"Wand.glb",
 	"Hat.glb",
 	"donut.glb",
-	"pumpkin.glb",	
-	"apple.glb",	
+	"pumpkin.glb",
+	"apple.glb",
 	"starboppers.glb",
 	"headphones.glb",
 ];
@@ -205,24 +205,12 @@ export const objectColours = {
 		"#E711CD",
 	],
 
-	"airplane.glb": [
-		"#E75D43",
-		"#6A6A6A",
-		"#E7C777",
-	],
+	"airplane.glb": ["#E75D43", "#6A6A6A", "#E7C777"],
 
-	"airplane2.glb": [
-		"#6A6A6A",
-		"#E75D43",
-		"#E7C777",
-	],
+	"airplane2.glb": ["#6A6A6A", "#E75D43", "#E7C777"],
 
-		"skateboard.glb": [
-		"#E769D3",
-		"#484848",
-		"#251BE7",
-	],
-	
+	"skateboard.glb": ["#E769D3", "#484848", "#251BE7"],
+
 	"rhino.glb": ["#6D6B6C", "#F6F6F6", "#373737", "#230F0F"],
 
 	"humped.glb": ["#FFA869", "#7E5024", "#E76F31"],
