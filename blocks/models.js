fb72d341e92d53915ed1b69df96cc76355b7a4dc
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2025-11-01 19:28:34 +0000

    Allow objects to have multiple colours in add block

--- blocks/models.js
+++ blocks/models.js
@@ -181,7 +181,7 @@ export function defineModelBlocks() {
 					{
 						type: "input_value",
 						name: "COLOR",
-						check: "Colour",
+						check:  ["Colour", "Array"],
 					},
 					{
 						type: "input_value",
