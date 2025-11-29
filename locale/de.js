03dae96e3b485933c2dad4606fa98c02b575d557
Zishan Rahman <zishanfrahman@outlook.com>
2025-11-29 21:10:04 +0000

    Add get_light block message and tooltip for German locale

--- locale/de.js
+++ locale/de.js
@@ -195,6 +195,7 @@ export default {
   // Custom block translations - Effects blocks
   light_intensity_and_color: "Lichtintensität auf %1 setzen %2 %3",
   set_fog: "Nebel setzen Farbe %1 Modus %2 Dichte %3\nStart %4 Ende %5",
+  get_camera: "Licht als %1 holen",
 
   // Custom block translation - Events blocks
   start: "start",
@@ -498,6 +499,7 @@ export default {
   // Effects tooltips
   light_intensity_and_color_tooltip: "Stelle die Intensität der Hauptbeleuchtung ein.\nSchlüsselwort: light intensity",
   set_fog_tooltip: "Konfiguriere den Nebel der Szene. Verwende Start und Ende, um die Nah- und Fernwerte festzulegen.\nSchlüsselwort: fog",
+  get_light_tooltip: "Hole die aktuelle Szene‑Hauptlicht.\nSchlüsselwort: fog",
 
   // Events tooltips
   start_tooltip: "Führe Blöcke beim Projektstart aus. Mehrere Startblöcke möglich.\nSchlüsselwort: start",
