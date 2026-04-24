bf41e0ab01f96c7a1c6797a490d908363e5df8e9
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2026-04-24 20:49:44 +0100

    Remove the demo menu from the tab order
    Prevent accidental creation of a new project.

--- main/input.js
+++ main/input.js
@@ -144,7 +144,7 @@ export function setupInput() {
         "#colorPickerButton",
         "#projectName",
         "#exportCodeButton",
-        "#exampleSelect",
+        //"#exampleSelect",
         "#toggleDesign",
         "#togglePlay",
         "#fullscreenToggle",
