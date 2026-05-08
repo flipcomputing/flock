3afcf02cb583353d3144f81586a2e59be3d0244f
Dr Tracy Gardner <tracygardner@users.noreply.github.com>
2026-05-08 15:45:57 +0000

    Keep same havok instance between runs

--- flock.js
+++ flock.js
@@ -1814,7 +1814,6 @@ export const flock = {
         // Dispose physics engine and release WASM heap
         flock.hk?.dispose();
         flock.hk = null;
-        flock.havokInstance = null;
 
         // Dispose the Babylon.js engine
         flock.engine?.dispose();
