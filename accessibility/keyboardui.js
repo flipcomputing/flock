288214d4be65ede4826d4babc87f024d2c632a2f
lawsie <5183697+lawsie@users.noreply.github.com>
2026-05-18 14:03:13 +0100

    Remove close button

--- accessibility/keyboardui.js
+++ accessibility/keyboardui.js
@@ -507,9 +507,10 @@ const ShortcutsPanel = {
     div.setAttribute("aria-label", translate("shortcut_panel_title"));
     div.tabIndex = 0;
     div.innerHTML = `
-        <a href="${SHORTCUTS_HELP_URL}" target="_blank" rel="noopener noreferrer" class="help-link-button" aria-label="Open keyboard shortcuts help page"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="16" height="16" aria-hidden="true"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l82.7 0L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3l0 82.7c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160c0-17.7-14.3-32-32-32L320 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z"/></svg></a>
-        <button type="button" class="close-button" id="closeShortcutsPanel" aria-label="${translate("shortcut_panel_close")}">&times;</button>
-        <h1 id="shortcuts-panel-title">${translate("shortcut_panel_title")}</h1>
+        <div class="shortcuts-panel-header">
+          <h1 id="shortcuts-panel-title">${translate("shortcut_panel_title")}</h1>
+          <a href="${SHORTCUTS_HELP_URL}" target="_blank" rel="noopener noreferrer" class="help-link-button" aria-label="Open keyboard shortcuts help page"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="16" height="16" aria-hidden="true"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l82.7 0L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3l0 82.7c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160c0-17.7-14.3-32-32-32L320 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z"/></svg></a>
+        </div>
         <table id="shortcuts-table"><tbody></tbody></table>
       `;
     document.getElementById("maincontent").appendChild(div);
@@ -521,9 +522,6 @@ const ShortcutsPanel = {
     this.panel.querySelector("#shortcuts-panel-title").textContent = translate(
       "shortcut_panel_title",
     );
-    this.panel
-      .querySelector("#closeShortcutsPanel")
-      .setAttribute("aria-label", translate("shortcut_panel_close"));
     const tbody = this.panel.querySelector("tbody");
     const groups = getShortcuts().reduce((acc, s) => {
       (acc[s.category] ??= []).push(s);
@@ -557,9 +555,6 @@ const ShortcutsPanel = {
   },
 
   setupListeners() {
-    document.addEventListener("click", (e) => {
-      if (e.target.id === "closeShortcutsPanel") this.hide();
-    });
     this.panel.addEventListener("keydown", (e) => {
       if (e.key === "ArrowUp") {
         e.preventDefault();
