0e58d34a81bfb16d85a55486773360dcd2640274
Tracy Gardner <14887827+tracygardner@users.noreply.github.com>
2026-05-31 12:23:32 +0100

    Update skip link to be first element

--- main/loading.js
+++ main/loading.js
@@ -1,3 +1,5 @@
+import { translate } from "./translation.js";
+
 // Function to hide loading screen
 export function hideLoadingScreen() {
   const loadingScreen = document.getElementById("loadingScreen");
@@ -7,6 +9,14 @@ export function hideLoadingScreen() {
     // First fade out loading screen
     loadingScreen.classList.add("fade-out");
     body.setAttribute("aria-busy", "false");
+
+    const announcements = document.getElementById("announcements");
+    if (announcements) {
+      announcements.textContent = "";
+      setTimeout(() => {
+        announcements.textContent = translate("loading_success_ui");
+      }, 20);
+    }
 
     // Then show main content after a brief delay
     setTimeout(() => {
